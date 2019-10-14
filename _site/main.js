8b810cdf3d0f7d76971be4b7a6b2101c79fa8f27
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2019-10-14 18:19:25 +0700

    initial commit

---
+++ _site/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  //click hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+      //show mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible')
+  });
+
+});
