8b406d2de3a72f1e8a81a4b39638dfde5c1e84eb
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2019-10-23 08:08:50 +0700

    emily's revisions plus smooth scroll

--- _site/main.js
+++ _site/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
-  //click hamburger icon
-  $('.toggle-mobile-menu').click(function(){
 
-      //show mobile menu
-      $('#mobile-menu').toggleClass('mobile-menu-visible')
-  });
+});
 
+//smooth scrolling better because rachel
+$('a[href^="#"]').on('click',function () {
+  var link = this.hash.substr(1);
+  var section = $('*[id=' + link + ']');
+
+  $('html,body').animate({
+    scrollTop: section.offset().top - 5
+  }, 800);
+  return false;
 });
